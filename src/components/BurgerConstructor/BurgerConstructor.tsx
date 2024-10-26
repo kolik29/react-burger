39ab72bc1cc02a273842a2d56584227c7710087a
kolik29 <kolik29@gmail.com>
2024-10-26 13:57:23 +0300

    add ConstructorElement, remove BurgerIngredient

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -1,33 +1,8 @@
-import { Button, CurrencyIcon, DeleteIcon, DragIcon, LockIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import React from 'react';
+import { Button, ConstructorElement, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './BurgerConstructor.module.css';
 import { Ingredient } from '../../types/Ingredient';
 import data from '../../utils/data.ts';
 import CustomScrollbar from '../CustomScrollbar/CustomScrollbar.tsx';
-
-const BurgerIngredient: React.FC<{ item: Ingredient, type?: string }> = ({ item, type }) => {
-  const isBun = type === 'buns-top' || type === 'buns-bottom';
-
-  return (
-    <div className={`${styles['burger-ingredient']} ${isBun ? styles[`burger-ingredient--${type}`] : ''} ${!isBun ? 'mb-4' : ''} position_relative display_grid align-items_center pl-6 pr-8 pt-4 pb-4 ml-8 mr-4`}>
-      {!isBun && <DragIcon type="primary" className={`${styles['burger-dragdrop']} cursor_pointer position_absolute`} />}
-      <div className="burger-ingredient-image">
-        <img src={item.image_mobile} alt="" width="80" height="40" />
-      </div>
-      <div className="burger-ingredient-name ml-5 mr-5 text text_type_main-default">
-        {item.name} {
-          type == 'buns-top' ? '(верх)' : type == 'buns-bottom' ? '(низ)' : ''
-        }
-      </div>
-      <div className="burger-ingredient-price mr-5">
-        <bdi className='display_flex align-items_center text text_type_digits-default'>{item.price} <CurrencyIcon type="primary" className='ml-3' /></bdi>
-      </div>
-      <div className="burger-ingredient-lock">
-        {isBun ? <LockIcon type="secondary" /> : <DeleteIcon type="primary" className="cursor_pointer" />}
-      </div>
-    </div>
-  )
-}
 
 const BurgerConstructor = () => {
   const buns = data[0];
@@ -36,15 +11,41 @@ const BurgerConstructor = () => {
   return (
     <section className={`${styles['burger-ingredients']} max-width_600px width_100 display_grid height_auto overflow_hidden mt-25`}>
       <div className={`display_grid ${styles.burger} overflow_hidden height_100`}>
-        <BurgerIngredient key={buns._id + '_top'} item={buns} type="buns-top" />
+        <div className="position_relative display_flex align-items_center justify-content_end pl-8 pr-4">
+          <ConstructorElement
+            type="top"
+            isLocked={true}
+            text={`${buns.name} (верх)`}
+            price={buns.price}
+            thumbnail={buns.image_mobile}
+            key={buns._id + '_top'}
+          />
+        </div>
         <div className="mt-4 mb-4 height_auto overflow_hidden">
           <CustomScrollbar>
             {ingredients.map((item: Ingredient, index: number) => (
-              <BurgerIngredient key={item._id + '-' + index} item={item} />
+              <div className={`${styles['burger-ingredient']} position_relative display_flex align-items_center justify-content_end pl-8 pr-4 pb-2 pt-2`}>
+                <DragIcon type="primary" className={`${styles['burger-dragdrop']} cursor_pointer position_absolute`} />
+                <ConstructorElement
+                  text={item.name}
+                  price={item.price}
+                  thumbnail={item.image_mobile}
+                  key={item._id + '_' + index}
+                />
+              </div>
             ))}
           </CustomScrollbar>
         </div>
-        <BurgerIngredient key={buns._id + '_bottom'} item={buns} type="buns-bottom" />
+        <div className="position_relative display_flex align-items_center justify-content_end pl-8 pr-4">
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text={`${buns.name} (низ)`}
+            price={buns.price}
+            thumbnail={buns.image_mobile}
+            key={buns._id + '_bottom'}
+          />
+        </div>
       </div>
       <div className="display_flex align-items_start mt-10 mb-10 pr-4">
         <div className="display_flex justify-content_end width_100">
