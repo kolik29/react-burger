377b4fa909c77161c31c33eba8eaae7defd856c2
kolik29 <kolik29@gmail.com>
2024-10-28 02:33:07 +0300

    update modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,25 +3,51 @@ import ReactDOM from 'react-dom';
 import styles from './Modal.module.css';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { iModal } from '../../types/Modal';
+import ModalOverlay from '../ModalOverlay/ModalOverlay';
 
-const Modal: React.FC<iModal> = ({ isOpen, onClose, children }: any) => {
+const Modal: React.FC<iModal> = ({ isModalOpen, onClose, children }: any) => {
   const modalRoot = document.getElementById('modal-root');
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsOpen(isModalOpen);
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isModalOpen, onClose]);
   
   if (!isOpen || !modalRoot) {
     return null;
   }
+
+  const onClickHandler = () => {
+    setIsOpen(false);
+    onClose();
+  }
   
   return ReactDOM.createPortal(
     (
-      <div className={styles.modal + ' position_fixed position_fixed--centered'}>
-        <div
-          className={styles.modal_close + ' cursor_pointer'}
-          onClick={onClose}
-        >
-          <CloseIcon type="primary" />
+      <>
+        <ModalOverlay isModalOpen={isModalOpen} onClose={onClose} />
+        <div className={`${styles.modal} position_fixed position_fixed--centered`}>
+          <div
+            className={`${styles.modal_close} cursor_pointer`}
+            onClick={onClickHandler}
+          >
+            <CloseIcon type="primary" />
+          </div>
+          {children}
         </div>
-        {children}
-      </div>
+      </>
     ), modalRoot
   );
 };
--- src/components/ModalOverlay/ModalOverlay.tsx
+++ src/components/ModalOverlay/ModalOverlay.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './ModalOverlay.module.css';
+import { iModal } from '../../types/Modal';
 
-const ModalOverlay: React.FC<{ isOpen: boolean, onClick: () => void }> = ({ isOpen, onClick }) => {
+const ModalOverlay: React.FC<iModal> = ({ isModalOpen, onClose }) => {
   const modalRoot = document.getElementById('modal-root');
-  
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsOpen(isModalOpen);
+  }, [isModalOpen]);
+
   if (!isOpen || !modalRoot) {
     return null;
+  }
+
+  const onClickHandler = () => {
+    setIsOpen(false);
+    onClose();
   }
   
   return ReactDOM.createPortal(
     (
-      <div className={styles.modal_overlay} onClick={onClick}></div>
+      <div className={styles.modal_overlay} onClick={onClickHandler}></div>
     ), modalRoot
   );
 };
