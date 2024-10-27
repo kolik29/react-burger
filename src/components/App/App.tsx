9643e07d0b1addf817794f7a809b691d4c43eb33
kolik29 <kolik29@gmail.com>
2024-10-28 02:34:01 +0300

    bugfix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import AppHeader from '../AppHeader/AppHeader';
 import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
-import Modal from '../Modal/Modal';
+import { iIngredient } from '../../types/Ingredient';
 
 function App() {
-  const [ingredients, setIngredients] = React.useState([])
+  const [ingredients, setIngredients] = React.useState<iIngredient[]>([])
   const url = 'https://norma.nomoreparties.space'
-    
+
   const fetchIngredients = React.useCallback(async () => {
     try {
       const response = await fetch(url + '/api/ingredients');
       
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
       const data = await response.json();
@@ -22,7 +22,7 @@ function App() {
     } catch (e) {
       console.error(e);
     }
-  }, [url])
+  }, [])
 
   React.useEffect(() => {
     fetchIngredients();
@@ -31,11 +31,11 @@ function App() {
   return (
     <>
       <AppHeader />
-      <div className='wrapper overflow_hidden height_100'>
-        <main className='container display_flex flex-direction_column height_100'>
+      <div className="wrapper overflow_hidden height_100">
+        <main className="container display_flex flex-direction_column height_100">
           <div className="display_flex justify-content_space-between height_100 overflow_hidden">
-            <BurgerIngredients data={ingredients} />
-            <BurgerConstructor data={ingredients} />
+            <BurgerIngredients key="ingredients" data={ingredients} />
+            <BurgerConstructor key="constructor" data={ingredients} />
           </div>
         </main>
       </div>
