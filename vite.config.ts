19171d9b05074dc479857a82bb02400a87668e7d
kolik29 <kolik29@gmail.com>
2025-01-22 15:06:48 +0300

    install vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+      exclude: [...configDefaults.exclude, 'src/**/*.d.ts'],
+    },
+  },
 })
